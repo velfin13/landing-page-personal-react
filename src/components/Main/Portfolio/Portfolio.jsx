4cefe49239cc267a3ad45c28be0557312ecfe3f2
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-04-28 16:58:39 -0500

    update

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -6,6 +6,7 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import ecommerce_games from "../../../assets/img/ecommerce_game_strapi_next.png";
 import css_flexbox from "../../../assets/img/css_flexbox.png";
 import tech_pro from "../../../assets/img/tech-pro-audifonos.png";
+import citas_medicas_vite from "../../../assets/img/citas_medicas_vite.png";
 
 import "./Portfolio.scss";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
@@ -51,6 +52,15 @@ const Portfolio = () => {
                 link_imagen={tech_pro}
                 link_source="https://github.com/velfin13/tech-pro-audifonos-page"
                 link_proyecto="https://velfin13.github.io/tech-pro-audifonos-page/"
+              />
+            </SplideSlide>
+            <SplideSlide>
+              <Proyectos
+                nombre="Proyecto de citas Veterinarias con react"
+                description="Un pequeño proyecto de citas hecho con React, Tailwind CSS y más"
+                link_imagen={citas_medicas_vite}
+                link_source="https://github.com/velfin13/administrador-de-citas-react-con-vite"
+                link_proyecto="https://citas-paciente-vite.netlify.app/"
               />
             </SplideSlide>
           </Splide>
